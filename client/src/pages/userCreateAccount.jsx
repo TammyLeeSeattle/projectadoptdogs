e50c6011f6fe5f989ae0088c25cd701b920a7b67
tammyleeseattle <tammylee84@gmail.com>
2019-05-26 14:18:55 -0700

    working on usercreateaccount page

--- client/src/pages/userCreateAccount.jsx
+++ client/src/pages/userCreateAccount.jsx
@@ -1,1 +1,15 @@
-// if we have time to refactor, I'd like for this page to be what's in Secret.js and UserInfo.js
+import React, { Component } from "react";  
+import UserInfo from "./../components/UserInfo";
+
+class App extends Component {  
+  render() {
+    return (
+      <div className="container">
+        <h3>React Form</h3>
+        <UserInfo />
+      </div>
+    );
+  }
+}
+
+export default App; 
