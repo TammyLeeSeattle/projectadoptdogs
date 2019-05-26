b44cbf0ac48570ff02130f47fc189a6e71329d23
tammyleeseattle <tammylee84@gmail.com>
2019-05-26 14:07:17 -0700

    adding the create account button

---
+++ client/src/pages/userCreateAccount.jsx
@@ -0,0 +1,1 @@
+// if we have time to refactor, I'd like for this page to be what's in Secret.js and UserInfo.js
---
+++ client/src/pages/welcome.jsx
@@ -0,0 +1,1 @@
+// if we have time to refactor, I'd like for this to be what's covered in Main.js
