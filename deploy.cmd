bd116a0492f78128dc35614654fd7331c213a8e5
tammyleeseattle <tammylee84@gmail.com>
2019-05-25 18:11:20 -0700

    troubleshooting cd for kudu 2.0

--- deploy.cmd
+++ deploy.cmd
@@ -108,8 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Build the website
 pushd "%DEPLOYMENT_TARGET%"
 echo "Building web site"
-call :ExecuteCmd cd client && !NPM_CMD! install --production
-call :ExecuteCmd cd client && !NPM_CMD! run build
+call :ExecuteCmd !NPM_CMD! run build
 if !ERRORLEVEL! NEQ 0 goto error
 popd
 
