6d394569666287226ac86e0e410cb2d0f3168b15
Lyly <yhl4340@gmail.com>
2019-05-26 17:25:19 -0700

    transparent nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,9 +2,10 @@ import React from "react";
 import Auth from "../Auth";
 
 function Nav(props) {
- console.log('nav', props);
-    return (
-      <div>
+  console.log("nav", props);
+  return (
+    <div className="navbar-fixed">
+      <nav className="transparent z-depth-0">
         <ul id="dropdown1" class="dropdown-content">
           <a href="#" data-target="mobile" class="sidenav-trigger">
             <i class="material-icons right">account_circle</i>
@@ -13,20 +14,20 @@ function Nav(props) {
             <a href="#!">one</a>
           </li>
           <li>
-              <a href="#" onClick={props.auth.logout}
-                className="white-text"
-                 id="logout"
-                >
-                 Log Out
-
-              </a>
+            <a
+              href="#"
+              onClick={props.auth.logout}
+              className='indigo-text text-darken-4'
+              id="logout"
+            >
+              Log Out
+            </a>
           </li>
-          
           <li class="divider" />
         </ul>
 
-        <nav className="grey lighten-1">
-          <div class="nav-wrapper">
+        <nav className="transparent z-depth-0">
+          <div class="navbar-fixed ">
             <a href="#" data-target="mobile-demo" class="sidenav-trigger">
               <i class="material-icons">menu</i>
             </a>
@@ -35,38 +36,33 @@ function Nav(props) {
             </a>
             <ul class="right hide-on-med-and-down">
               <li>
-                <a href="sass.html">About Us</a>
+                <a href="sass.html" class='indigo-text text-darken-4'>About Us</a>
               </li>
               {/* <li><a class="dropdown-trigger" href="#!" data-target="dropdown1"><i class="medium material-icons right">account_circle</i></a></li> */}
 
-              {!props.auth.isAuthenticated() ? 
+              {!props.auth.isAuthenticated() ? (
                 <li>
                   <a
                     href="#"
                     onClick={props.auth.login}
-                    className="white-text"
+                    className='indigo-text text-darken-4'
                     id="login"
                   >
                     Log In
                   </a>
                 </li>
-               : 
+              ) : (
                 <li>
-                  <a
-                    class="dropdown-trigger"
-                    href="#!"
-                    data-target="dropdown1"
-                  >
-                    <i class="medium material-icons right">
-                      account_circle
-                    </i>
+                  <a class="dropdown-trigger" href="#!" data-target="dropdown1">
+                    <i class="medium material-icons right">account_circle</i>
                   </a>
                 </li>
-              }
+              )}
             </ul>
           </div>
         </nav>
-      </div>
-    );
-  }
+      </nav>
+    </div>
+  );
+}
 export default Nav;
