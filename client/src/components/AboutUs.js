d63788bb393f6cae4d8fedcc79a4c91bf645497d
tammyleeseattle <tammylee84@gmail.com>
2019-06-01 10:23:53 -0700

    providing content in about us

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -7,20 +7,23 @@ function AboutUs (){
         <div class="col s12 m7">
           <div class="card">
             <div class="card-image">
-              <img src="images/sample-1.jpg"/>
-              <span class="card-title">Card Title</span>
+              {/* <img src="images/sample-1.jpg"/> */}
+              <span class="card-title"></span>
             </div>
             
             <div class="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">This is a link</a>
-            </div>
+              <p>About Us!</p>
+              <hr></hr>
+              <p>We are a two-person team in the University of Washington's Programming Bootcamp.</p>
+              <br></br>
+              <p>Yin Lee</p>
+              <p><a href="https://www.linkedin.com/in/yin-lee-517087171/" target="_blank">LinkedIn Profile</a></p>
+              <p>Tammy Lee</p>
+              <p><a href="https://www.linkedin.com/in/tammymleewa/" target="_blank">LinkedIn Profile</a></p>
           </div>
         </div>
-      </div>         
+      </div>
+      </div>     
     )
 }
 export default AboutUs;
